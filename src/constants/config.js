ada68a482aaf03a79830c37b837ddffaaa663089
Ishara Dhanushan <109136246+isharadh2002@users.noreply.github.com>
2026-03-18 16:41:13 +0530

    Fixing remote url

--- src/constants/config.js
+++ src/constants/config.js
@@ -1,5 +1,5 @@
-// export const HUB_URL = "https://extensions-hub-theta.vercel.app";
-export const HUB_URL = "http://localhost:3000";
+export const HUB_URL = "https://extensions-hub-theta.vercel.app";
+// export const HUB_URL = "http://localhost:3000";
 export const APP_ID = "fullgrab";
 export const TRIAL_DURATION_DAYS = 20;
 export const ENTITLEMENT_CACHE_TTL = 120000; // 2 minutes cache time
